cb866d2f1439b949604c6bfafccdc2df2e265c9c
prathamrasal <prathamrasal6@gmail.com>
2022-04-26 14:57:34 +0530

    Listening Section Completed

--- app/src/main/java/com/example/happyhours/listening.java
+++ app/src/main/java/com/example/happyhours/listening.java
@@ -2,7 +2,9 @@ package com.example.happyhours;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,34 +15,47 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class listening extends AppCompatActivity {
-    ListView listView;
-    ArrayList<String> arrayList;
-    ArrayAdapter adapter;
-    MediaPlayer Player;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listening);
 
-        listView = findViewById(R.id.listview);
-        arrayList = new ArrayList<String>();
-        Field[] fields = R.raw.class.getFields();
-        for (int i = 0 ; i<fields.length;i++){
-            arrayList.add(fields[i].getName());
-        }
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-             if (Player!=null){
-                 Player.release();
-             }
-             int resId = getResources().getIdentifier(arrayList.get(i),"raw",getPackageName());
-             Player = MediaPlayer.create(listening.this,resId);
-                Player.start();
-            }
-        });
+    }
+
+    public void englishlofi(View view) {
+        gotoUrl("https://youtu.be/8wKRURAxwyw");
+    }
+
+    public void arrehman(View view) {
+        gotoUrl("https://youtu.be/_Or4pg-N58M");
+    }
+
+    public void english(View view) {
+        gotoUrl("https://youtu.be/5mFTXbZzOAE");
+    }
+
+    public void bollywood(View view) {
+        gotoUrl("https://youtu.be/q_FJGihX2ow");
+    }
+
+    public void atifaslam(View view) {
+        gotoUrl("https://youtu.be/k_ZNTA1SQ2M");
+    }
+
+    public void arijitsingh(View view) {
+        gotoUrl("https://youtu.be/0s10Trf8u9w");
+    }
+
+    public void rahat(View view) {
+        gotoUrl("https://youtu.be/CBVJTplw4cE");
+    }
+
+    public void jubin(View view) {
+        gotoUrl("https://youtu.be/TDHYP0N44hg");
+    }
+
+    private void gotoUrl(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
     }
 }
